bbae62c7e37442e10a944c61f6a1bc463e8c4f24
BACK-0205 <back-0205@Erxess-Mac-mini.local>
2025-10-16 18:45:04 +0800

    atm bodlogo

---
+++ index.js
@@ -0,0 +1,12 @@
+const username = window.prompt("Nevtreh ner");
+const password = window.prompt("Nuuts ug");
+
+const uldegdel = Math.floor(Math.random() * 10000000) + 10000;
+
+console.log(uldegdel);
+
+const nemeh = Number(prompt("nemeh mungu"));
+
+const avah = Number(prompt("avah mungu"));
+
+console.log( (uldegdel + nemeh ) - avah);
