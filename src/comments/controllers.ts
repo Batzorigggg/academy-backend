30988d82ed757aee9e875d507916c8f84fa9867a
BACK-0205 <back-0205@Erxess-Mac-mini.local>
2026-02-23 19:40:25 +0800

    a

--- src/comments/controllers.ts
+++ src/comments/controllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { Comments } from "./models.ts";
 
 export const Commentsontroller = async (req: Request, res: Response) => {
-  const comment = await Comments.findOne({});
+  const comment = await Comments.find({});
   res.send(comment);
 };
 export const CommentsIdController = async (req: Request, res: Response) => {
--- src/comments/routes.ts
+++ src/comments/routes.ts
@@ -3,6 +3,25 @@ import { Comments } from "./models";
 
 export const commentRouter = Router();
 
-commentRouter.get("/comments", async (req: Request, res: Response) => {
-  const comment = await Comments.find({}).limit(100);
+commentRouter.get(
+  "movies/:_id/comments",
+  async (req: Request, res: Response) => {
+    const movie_id = "573a1390f29313caabcd446f";
+    const comment = await Comments.find({ movie_id: movie_id });
+    console.log(comment);
+    return comment;
+  },
+);
+
+commentRouter.post("movies/comments", async (req: Request, res: Response) => {
+  // const movie_id = "573a1390f29313caabcd446f";
+  const { email, id, text } = req.body;
+  // const comment = await Comments.find({ movie_id: movie_id });
+
+
+  const ddd= await Comments.insertOne({
+ req.body
+  }) 
+  console.log(comment);
+  return comment;
 });
